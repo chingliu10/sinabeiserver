ca13d8773885d85fa82fa9a68c73956ee2a2509a
chingliu10 <chingliu10@gmail.com>
2021-05-13 13:24:43 +0300

    revived config for local and remote

--- database.js
+++ database.js
@@ -1,16 +1,8 @@
 const mysql = require("mysql2")
 
-// let connection = mysql.createConnection({
-//     host : "208.117.86.203" ,
-//     user : "sinabei",
-//     password : "fungaMeza12?",
-//     database : "pos",
-//     port : 3306
-// })
-
 let connection = mysql.createConnection({
-    host : "localhost" ,
-    user : "root",
+    host : "208.117.86.203" ,
+    user : "sinabei",
     password : "fungaMeza12?",
     database : "pos",
     port : 3306
