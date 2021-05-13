b086a341d36ce562e1f1692c51db61235ddf8d4d
chingliu10 <chingliu10@gmail.com>
2021-05-13 13:23:19 +0300

    changed the db config

--- database.js
+++ database.js
@@ -1,8 +1,16 @@
 const mysql = require("mysql2")
 
+// let connection = mysql.createConnection({
+//     host : "208.117.86.203" ,
+//     user : "sinabei",
+//     password : "fungaMeza12?",
+//     database : "pos",
+//     port : 3306
+// })
+
 let connection = mysql.createConnection({
-    host : "208.117.86.203" ,
-    user : "sinabei",
+    host : "localhost" ,
+    user : "root",
     password : "fungaMeza12?",
     database : "pos",
     port : 3306
