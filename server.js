3ea9e095584e9a6953d7abefef882e88f31a2e0e
chingliu10 <chingliu10@gmail.com>
2021-05-13 14:22:19 +0300

    allowed cross origin for nodejs

--- server.js
+++ server.js
@@ -9,6 +9,11 @@ app.use(express.static("public"))
 app.set("view engine", "hbs")
 hbs.registerPartials(__dirname + "/views/partials")
 
+app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+  })
 app.use("/", home)
 app.use("/admin", admin)
 app.use("/dashboard", dashboard)
