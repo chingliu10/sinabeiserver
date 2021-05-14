88b7d8a2015e2d3c250e43d6d87c7486c80bd27b
chingliu10 <chingliu10@gmail.com>
2021-05-14 15:25:07 +0300

    added a file with window

--- routes/dashboard.js
+++ routes/dashboard.js
@@ -5,7 +5,7 @@ let multer = require("multer")
 
 let storage = multer.diskStorage({
     destination : function (req, file, cb) {
-        cb(null, 'sinabeiserver/public/images')
+        cb(null, 'public/images')
     },
     filename : function (req, file, cb) {
         cb(null, file.fieldname + '_' + Date.now())
